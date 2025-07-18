d9335c4a527019912b6d97ad3517a136118d5d9b
sandeep <sandeepsanu1230@gmail.com>
2025-07-18 21:57:38 +0530

    cpp compiler

---
+++ compiler/generateFile.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+const path = require("path");
+const { v4: uuid } = require("uuid");
+
+const dirCodes = path.join(__dirname, "codes");
+
+if (!fs.existsSync(dirCodes)) {
+  fs.mkdirSync(dirCodes, { recursive: true });
+}
+
+const generateFile = (format, content) => {
+  const jobId = uuid();
+  const fileName = `${jobId}.${format}`;
+  const filePath = path.join(dirCodes, fileName);
+  fs.writeFileSync(filePath, content);
+  return filePath;
+};
+
+module.exports = { generateFile };
